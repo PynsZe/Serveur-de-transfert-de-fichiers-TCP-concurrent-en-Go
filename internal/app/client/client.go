a9e20a8520fd3c4fd77f3a909dfcf77391eab3fb
Mathis TONG--HATET <mathistonghatet@gmail.com>
2025-12-03 16:06:24 +0100

    init logique client

--- internal/app/client/client.go
+++ internal/app/client/client.go
@@ -1,8 +1,12 @@
 package client
 
 import (
+	"bufio"
+	"fmt"
 	"log/slog"
 	"net"
+	"os"
+	"strings"
 )
 
 func Run(remote string) {
@@ -12,11 +16,42 @@ func Run(remote string) {
 		slog.Error(e.Error())
 		return
 	}
+
 	defer func() {
 		c.Close()
 		slog.Debug("Connection closed")
 	}()
+	
 	slog.Info("Connected to " + c.RemoteAddr().String())
 
-	return
+
+	in := bufio.NewReader(c)
+	out := bufio.NewWriter(c)
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("commande : ")
+		line, _ := stdin.ReadString('\n')
+        line = strings.TrimSpace(line)
+
+		_, err := out.WriteString(line + "\n")
+		if (err != nil){
+			slog.Error(e.Error())
+			return
+		}
+		out.Flush()
+
+		if (line == "end"){
+			slog.Info("Session closed by user")
+			return
+		}
+
+		resp, err := in.ReadString('\n')
+		if (err != nil){
+			slog.Error(e.Error())
+			return
+		}
+
+		fmt.Println(resp)
+	}
 }
